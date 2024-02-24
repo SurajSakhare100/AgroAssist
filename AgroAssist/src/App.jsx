95bbc6582667cd5f3448720c1a5290ca7710012f
suraj sakhare <sakharesuraj10@gmail.com>
2024-02-24 10:29:48 +0530

    intial commit

---
+++ AgroAssist/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-red-300'>Agro Assit</h1>
+    </div>
+  )
+}
+
+export default App
